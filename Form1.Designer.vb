c5a256fee212b53b2b8a3a7ae301b86fdd2343c6
Dell-User <Dell-User@DESKTOP-M748B2N>
2023-10-05 12:59:53 +0200

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,98 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        btnCompute = New Button()
+        Label1 = New Label()
+        Label2 = New Label()
+        TextBox2 = New TextBox()
+        ComboBox1 = New ComboBox()
+        SuspendLayout()
+        ' 
+        ' btnCompute
+        ' 
+        btnCompute.Location = New Point(327, 265)
+        btnCompute.Name = "btnCompute"
+        btnCompute.Size = New Size(75, 23)
+        btnCompute.TabIndex = 0
+        btnCompute.Text = "Compute"
+        btnCompute.UseVisualStyleBackColor = True
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(239, 88)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(117, 15)
+        Label1.TabIndex = 1
+        Label1.Text = "Enter the grade letter"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(239, 143)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(39, 15)
+        Label2.TabIndex = 1
+        Label2.Text = "Result"
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.Location = New Point(409, 140)
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(100, 23)
+        TextBox2.TabIndex = 2
+        ' 
+        ' ComboBox1
+        ' 
+        ComboBox1.AllowDrop = True
+        ComboBox1.DropDownStyle = ComboBoxStyle.DropDownList
+        ComboBox1.FormattingEnabled = True
+        ComboBox1.Items.AddRange(New Object() {"A", "A-", "B", "C", "D", "E", "F"})
+        ComboBox1.Location = New Point(409, 88)
+        ComboBox1.Name = "ComboBox1"
+        ComboBox1.Size = New Size(121, 23)
+        ComboBox1.TabIndex = 3
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(ComboBox1)
+        Controls.Add(TextBox2)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Controls.Add(btnCompute)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents btnCompute As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents ComboBox1 As ComboBox
+End Class
---
+++ Form1.vb
@@ -0,0 +1,25 @@
+﻿Public Class Form1
+    Private Sub btnCompute_Click(sender As Object, e As EventArgs) Handles btnCompute.Click
+        Dim Grade As String
+        Grade = ComboBox1.Text
+
+        Select Case Grade
+            Case "A"
+                TextBox2.Text = "High Distinction"
+            Case "A-"
+                TextBox2.Text = "Distinction"
+            Case "B"
+                TextBox2.Text = "Credit"
+            Case "C"
+                TextBox2.Text = "Pass"
+            Case Else
+                TextBox2.Text = "Fail"
+        End Select
+
+
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+End Class
